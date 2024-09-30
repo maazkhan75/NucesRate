04a39538638b72e6e5130ed2825c4149aa5855eb
maazkhan75 <maazkhan75555@gmail.com>
2024-09-30 23:43:34 +0500

    google auth partial work

--- app/actions.ts
+++ app/actions.ts
@@ -19,7 +19,7 @@ export async function googleAuthSignIn(provider: Provider) {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
     options: {
-      redirectTo: 'https://acadlens.vercel.app/auth/callback'
+      redirectTo: 'https://nuceshub.vercel.app/auth/callback'
     },
   })
 
