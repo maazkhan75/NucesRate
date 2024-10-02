82f10c6a8bbb7a6d1fd398bfe5b400611c44dacf
maazkhan75 <maazkhan75555@gmail.com>
2024-10-02 23:41:05 +0500

    frontend level++

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -3,7 +3,8 @@ import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/server";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from "./ui/dropdown-menu";
-import GoogleSignInButton from "./ui/google_sign_in";
+import Link from "next/link";
+import { googleAuthSignIn } from "@/app/actions";
 import Image from "next/image";
  
 export default async function AuthButton() {
@@ -13,14 +14,15 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email?.slice(0, user.email?.indexOf('@'))}
-
+      Hey, {user.email?.slice(0, user.email?.indexOf("@"))}
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="outline">
             <Avatar>
-              <AvatarImage src={user?.user_metadata.avatar_url} sizes="100"  />
-              <AvatarFallback>{user.email?.slice(0, user.email?.indexOf('@'))}</AvatarFallback>
+              <AvatarImage src={user?.user_metadata.avatar_url} sizes="100" />
+              <AvatarFallback>
+                {user.email?.slice(0, user.email?.indexOf("@"))}
+              </AvatarFallback>
             </Avatar>
           </Button>
         </DropdownMenuTrigger>
@@ -31,11 +33,12 @@ export default async function AuthButton() {
               Sign out
             </Button>
           </form>
-          </DropdownMenuContent>
+        </DropdownMenuContent>
       </DropdownMenu>
-
     </div>
   ) : (
-    <GoogleSignInButton />
+    <Button onClick={() => googleAuthSignIn("google")} variant="outline">
+      Sign in with Google
+    </Button>
   );
 }
