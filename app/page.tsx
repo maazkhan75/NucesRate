9218c548591297e91888218985b0fdb34b17e16d
maazkhan75 <maazkhan75555@gmail.com>
2024-10-02 14:06:10 +0500

    bug removed!

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import {Navbar} from "@/components/ui/navbar"
 import { Hero } from "@/components/landing_page/hero";
 import { ProductShowcaseAndFAQs } from "@/components/landing_page/ProductShowcaseAndFAQs";
 import { Features } from "@/components/landing_page/Features";
-import { FAQs } from "@/components/landing_page/FAQs";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
