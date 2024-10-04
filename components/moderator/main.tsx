3d5b762c507f17c678dd87b1cf3e4e527d8a916d
afaqkhaliq <afaqkhaliq@gmail.com>
2024-10-04 23:24:03 +0500

    pending Review page and fetch main page added

--- components/moderator/main.tsx
+++ components/moderator/main.tsx
@@ -1,19 +1,44 @@
 import './main.css';
-import Sidebar from '@/components/moderator/sidebar'
-const main=()=>{
-    return (
-        <div className="layout">
-          <Sidebar/>
-          <div className="container">
-            <div className="Card">
-              <h1>LHR</h1>
-              <h2><a href='#'>Reviews Pending:500</a></h2>
-              <h2> <a href='#'>Professor Request:10</a></h2>
-              <h2><a href='#'>Courses Request:10</a></h2>
-            </div>
-          </div>
-        </div>
-      );
-}
+import Sidebar from '@/components/moderator/sidebar';
+import { createClient } from '@/utils/supabase/client';
 
-export default main;
+const Main = async () => {
+  const supabase = createClient();
+
+  const [reviewsResponse, professorsResponse, courseResponse] = await Promise.all([
+    supabase.from('reviews').select('*', { count: 'exact' }).is('approved', null),
+    supabase.from('request_professor').select('*', { count: 'exact' }).is('status', null),
+    supabase.from('request_course').select('*', { count: 'exact' }).is('status', null),
+  ]);
+
+  if (reviewsResponse.error || professorsResponse.error || courseResponse.error) {
+    const errorMessage = reviewsResponse.error?.message || professorsResponse.error?.message || courseResponse.error?.message;
+    return <div>Error: {errorMessage}</div>;
+  }
+
+  const pendingReviewsCount = reviewsResponse.count || 0;
+  const professorRequestCount = professorsResponse.count || 0;
+  const courseRequestCount = courseResponse.count || 0;
+
+  return (
+    <div className="layout">
+      <Sidebar />
+      <div className="container">
+        <div className="Card">
+          <h1>LHR</h1>
+          <h2>
+            <a href="moderator/pendingReviews">Reviews Pending: {pendingReviewsCount}</a>
+          </h2>
+          <h2>
+            <a href="#">Professor Request: {professorRequestCount}</a>
+          </h2>
+          <h2>
+            <a href="#">Courses Request: {courseRequestCount}</a>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
