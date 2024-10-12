4a6c638d3bb331e5144c2e3930cc4aec989a40c1
Mohsin <mohsinkamaldev@gmail.com>
2024-10-12 10:37:24 +0500

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -9,44 +9,19 @@ export async function GET(request: Request) {
   if (code) {
     const supabase = createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
-    if (!error) {
-      const { data: { user } } = await supabase.auth.getUser();
-
-      if (user) {
-        const email = user.email;
-
-        const { data: existingStudent, error: fetchError } = await supabase
-          .from('students')
-          .select('stud_id')
-          .eq('stud_email', email)
-          .single();
-
-        if (fetchError && fetchError.code !== 'PGRST116') {
-          console.error('Error fetching student:', fetchError);
+    if (error) {
+          console.error('Error Initializing Cookies');
           return NextResponse.redirect(`${origin}/auth/error`);
-        }
-
-        if (!existingStudent) {
-          const { error: insertError } = await supabase
-            .from('students')
-            .insert([{ stud_email: email }]); 
-
-          if (insertError) {
-            console.error('Error inserting student:', insertError);
-            return NextResponse.redirect(`${origin}/auth/error`);
-          }
-        }
-      }
+    }
   
-      const forwardedHost = request.headers.get('x-forwarded-host')
-      const isLocalEnv = process.env.NODE_ENV === 'development'
-      if (isLocalEnv) {
-        return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
-      } else {
-        return NextResponse.redirect(`${origin}${next}`)
-      }
+    const forwardedHost = request.headers.get('x-forwarded-host')
+    const isLocalEnv = process.env.NODE_ENV === 'development'
+    if (isLocalEnv) {
+      return NextResponse.redirect(`${origin}${next}`)
+    } else if (forwardedHost) {
+      return NextResponse.redirect(`https://${forwardedHost}${next}`)
+    } else {
+      return NextResponse.redirect(`${origin}${next}`)
     }
   }
 
