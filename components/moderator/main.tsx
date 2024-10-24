37357589c2b5d9913aa90cf1796cf2a1baac1781
maazkhan75 <maazkhan75555@gmail.com>
2024-10-25 00:52:43 +0500

    loaders

--- components/moderator/main.tsx
+++ components/moderator/main.tsx
@@ -1,18 +1,35 @@
-import './main.css';
-import Sidebar from '@/components/moderator/sidebar';
-import { createClient } from '@/utils/supabase/client';
+import "./main.css";
+import Sidebar from "@/components/moderator/sidebar";
+import { createClient } from "@/utils/supabase/client";
 
 const Main = async () => {
   const supabase = createClient();
 
-  const [reviewsResponse, professorsResponse, courseResponse] = await Promise.all([
-    supabase.from('reviews').select('*', { count: 'exact' }).is('approved', null),
-    supabase.from('request_professor').select('*', { count: 'exact' }).is('status', null),
-    supabase.from('request_course').select('*', { count: 'exact' }).is('status', null),
-  ]);
+  const [reviewsResponse, professorsResponse, courseResponse] =
+    await Promise.all([
+      supabase
+        .from("reviews")
+        .select("*", { count: "exact" })
+        .is("approved", null),
+      supabase
+        .from("request_professor")
+        .select("*", { count: "exact" })
+        .is("status", null),
+      supabase
+        .from("request_course")
+        .select("*", { count: "exact" })
+        .is("status", null),
+    ]);
 
-  if (reviewsResponse.error || professorsResponse.error || courseResponse.error) {
-    const errorMessage = reviewsResponse.error?.message || professorsResponse.error?.message || courseResponse.error?.message;
+  if (
+    reviewsResponse.error ||
+    professorsResponse.error ||
+    courseResponse.error
+  ) {
+    const errorMessage =
+      reviewsResponse.error?.message ||
+      professorsResponse.error?.message ||
+      courseResponse.error?.message;
     return <div>Error: {errorMessage}</div>;
   }
 
@@ -27,7 +44,9 @@ const Main = async () => {
         <div className="Card">
           <h1>LHR</h1>
           <h2>
-            <a href="moderator/pendingReviews">Reviews Pending: {pendingReviewsCount}</a>
+            <a href="moderator/pendingReviews">
+              Reviews Pending: {pendingReviewsCount}
+            </a>
           </h2>
           <h2>
             <a href="#">Professor Request: {professorRequestCount}</a>
